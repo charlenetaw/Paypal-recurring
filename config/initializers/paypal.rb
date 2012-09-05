30ccb6c5279611c0f9ed3bbfc600a543ab261cfb
Charlene Taw <charlene_taw@yahoo.com>
2012-09-05 17:22:16 +0800

    first commit

---
+++ config/initializers/paypal.rb
@@ -0,0 +1,6 @@
+PayPal::Recurring.configure do |config|
+  config.sandbox = true
+  config.username = "cha_1326954053_biz_api1.appsource.biz"
+  config.password = "1326954078"
+  config.signature = "AFcWxV21C7fd0v3bYYYRCpSSRl31AIV9LPquzPrSY0FK6i7QdQiYYTNk"
+end
