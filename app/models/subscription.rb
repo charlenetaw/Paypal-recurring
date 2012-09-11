8dffbdfb49a8cc4980f800a3aeb4f8b2c0af599d
markaventura <maventura@appsource.biz>
2012-09-11 09:11:33 +0800

    button

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -25,15 +25,15 @@ class Subscription < ActiveRecord::Base
     save!
   end
 
-  def save_with_stripe_payment
-    customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
-    self.stripe_customer_token = customer.id
-    save!
-  rescue Stripe::InvalidRequestError => e
-    logger.error "Stripe error while creating customer: #{e.message}"
-    errors.add :base, "There was a problem with your credit card."
-    false
-  end
+  #def save_with_stripe_payment
+    #customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+    #self.stripe_customer_token = customer.id
+    #save!
+  #rescue Stripe::InvalidRequestError => e
+    #logger.error "Stripe error while creating customer: #{e.message}"
+    #errors.add :base, "There was a problem with your credit card."
+    #false
+  #end
   
   def payment_provided?
     stripe_card_token.present? || paypal_payment_token.present?
